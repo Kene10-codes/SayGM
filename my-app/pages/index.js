9d846b1d3a48ccd40d259e4dfaa6f141db8c1704
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-10-30 14:30:53 +0100

    deployed app to Goerli

---
+++ my-app/pages/index.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import GmForm from "../components/GmForm";
+import styles from "../styles/Home.module.css";
+import abi from "../utils/abi.json";
+import { ethers } from "ethers";
+
+const contractAddress = "";
+
+export default function Home() {
+  const [address, setAddress] = useState("");
+  const [gmContract, setGmContract] = useState({});
+  const handleWalletConnect = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setAddress(address);
+      const connectedContract = new ethers.Contract(
+        contractAddress,
+        abi.abi,
+        signer
+      );
+      setGmContract(connectedContract);
+    } else {
+      alert("No Wallet Detected");
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        {address ? (
+          <GmForm address={address} gmContract={gmContract} />
+        ) : (
+          <button onClick={() => handleWalletConnect()}>Connect Wallet</button>
+        )}
+      </main>
+    </div>
+  );
+}
